7a218818a6c297da6b543cc0934fc63439696de4
asekakasundi <aseka.kasundi@gmail.com>
2026-05-04 21:27:26 +0530

    Implemented viewStudents feature

--- Main.java
+++ Main.java
@@ -8,11 +8,20 @@ public class Main {
 
         addStudent("Alice");
         addStudent("Bob");
+
+        viewStudents();
     }
 
     static void addStudent(String name) {
         students.add(name);
         System.out.println(name + " added.");
+    }
+
+    static void viewStudents() {
+        System.out.println("Student List:");
+        for (String s : students) {
+            System.out.println(s);
+        }
     }
 
 }
