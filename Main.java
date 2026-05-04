e35474d7894bf35a2365023d21f56517bd5dfb86
asekakasundi <aseka.kasundi@gmail.com>
2026-05-04 21:06:29 +0530

    Implemented addStudent feature

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
 public class Main {
+
     static ArrayList<String> students = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Student Record Manager");
+
+        addStudent("Alice");
+        addStudent("Bob");
+    }
+
+    static void addStudent(String name) {
+        students.add(name);
+        System.out.println(name + " added.");
     }
 
 }
